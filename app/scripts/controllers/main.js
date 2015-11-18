e9e421b9734b1b4cf9c283a518569f53e1d04f5a
ajchan11 <ajchan11@gmail.com>
2015-11-17 16:52:56 -0800

    testing

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name soundmind2App.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the soundmind2App
+ */
+angular.module('soundmind')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
